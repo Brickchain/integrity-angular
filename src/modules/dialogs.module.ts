fdf33691ac99c9a5f0019f8f09b4e660a3973655
Erik Hedenström <erik@hedenstroem.com>
2018-02-20 10:51:02 +0100

    Better imports

--- src/modules/dialogs.module.ts
+++ src/modules/dialogs.module.ts
@@ -1,7 +1,8 @@
 import { NgModule, } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule, MatButtonModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { QRCodeModule } from './qrcode.module';
