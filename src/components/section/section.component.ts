6a97513006d615a0a2a8711d7a10681ceca53034
Erik Hedenström <erik@hedenstroem.com>
2018-04-17 17:19:42 +0200

    Working on collapse

--- src/components/section/section.component.ts
+++ src/components/section/section.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'integrity-section',
   templateUrl: './section.component.html',
-  styleUrls: ['./section.component.css']
+  styleUrls: ['./section.component.scss']
 })
 export class SectionComponent implements OnInit {
 
   @Input() public title = '';
+  @Input() public collapsible = false;
+  @Input() public cid = '';
+
+  public collapsed = false;
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.title, this.cid, this.collapsible);
+  }
+
+  toggle() {
+    this.collapsed = !this.collapsed;
+    console.log(this.collapsed);
   }
 
 }
