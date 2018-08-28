c7025b148c97b1dddf49542b77eabfc8c806a7a9
Jesper Brännström <jesper@brickchain.com>
2018-08-28 14:18:10 +0200

    Remove unused modules in schedule module. Conform to how material design modules are imported in other modules.

--- src/modules/schedule.module.ts
+++ src/modules/schedule.module.ts
@@ -1,25 +1,22 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSelectModule } from '@angular/material/select';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule, MatInputModule, MatCheckboxModule, MatButtonModule } from '@angular/material';
 import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
-import { ScheduleComponent } from '../components/schedule/schedule.component';
+import { FormsModule } from '@angular/forms';
+import {
+  MatIconModule,
+  MatSelectModule,
+  MatInputModule,
+  MatCheckboxModule,
+  MatButtonModule } from '@angular/material';
 import { OwlDateTimeModule } from 'ng-pick-datetime';
 import { OwlMomentDateTimeModule } from 'ng-pick-datetime-moment';
+import { ScheduleComponent } from '../components/schedule/schedule.component';
 
 @NgModule({
     imports: [
         MatIconModule,
         FormsModule,
-        ReactiveFormsModule,
         MatSelectModule,
         CommonModule,
-        BrowserModule,
-        MatDatepickerModule,
-        MatNativeDateModule,
         MatInputModule,
         OwlDateTimeModule,
         OwlMomentDateTimeModule,
